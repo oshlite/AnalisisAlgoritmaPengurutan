8e0e5ba6d60b5567585ccc619142a2f23c731b3d
lite version of me || oryza surya <140532148+oshlite@users.noreply.github.com>
2024-06-26 21:19:21 +0700

    Update all Sort done.cpp

--- all Sort done.cpp
+++ all Sort done.cpp
@@ -28,3 +28,15 @@ void Insertion(vector<int>& arr){
     }
 }
 
+void Selection(vector<int>& arr){
+    int n=arr.size();
+    for(int i=0; i<n-1; i++){
+        int indexMin=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[indexMin]){
+                indexMin=j;
+            }
+        }swap(arr[i],arr[indexMin]);
+    }
+}
+
